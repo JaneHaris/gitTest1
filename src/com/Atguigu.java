06e255a71fbb9491c2a8b56f26a5dce8172f1307
Elio Donald <45617207+Zhaochuanyun1@users.noreply.github.com>
2022-02-16 11:53:21 +0800

    Update Atguigu.java
    试试

--- src/com/Atguigu.java
+++ src/com/Atguigu.java
@@ -2,6 +2,6 @@ package com;
 
 public class Atguigu {
     public static void main(String[] args) {
-        System.out.println("hello world111");
+        System.out.println("hello world1112222");
     }
 }
